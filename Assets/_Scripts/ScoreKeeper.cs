9e0b4576502c9ca0bb925beb9bfa96a08facd1df
DARSHEEL-PC\darsh <darsheel.techdev@gmail.com>
2023-06-15 22:37:52 +0530

    New Update

--- Assets/_Scripts/ScoreKeeper.cs
+++ Assets/_Scripts/ScoreKeeper.cs
@@ -14,6 +14,7 @@ public class ScoreKeeper : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        Debug.Log("Save mat karna");
 
         // Set to this instance
         instance = this;
